3e2773209ed39aca28cc606319725ece1d438e66
Jordan Flash <flash.jordan.3@gmail.com>
2022-08-05 15:47:09 +0100

    kaz came once again

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.listen(process.env.PORT);
+// app.listen(process.env.PORT);
 
 
 app.use("/events", eventsRouter);
