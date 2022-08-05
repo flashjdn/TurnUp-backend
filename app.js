8d38ffd8030fa71504e4c1d75561abfd4df4726e
Jordan Flash <flash.jordan.3@gmail.com>
2022-08-05 15:17:28 +0100

    updated start script to include dot env config

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.listen(process.env.PORT);
 
+
 app.use("/events", eventsRouter);
 
 app.use(function (req, res, next) {
