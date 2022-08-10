5fbe149ccb7f5533cc887068218dcb92ded2924f
Jordan Flash <flash.jordan.3@gmail.com>
2022-08-10 10:34:27 +0100

    removed a pesky curly bracket

--- models/events.js
+++ models/events.js
@@ -51,7 +51,7 @@ export async function createEvent(request) {
           address, 
           img, 
           email
-        };)
+        )
         VALUES 
         ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11 ) 
         RETURNING *;`,
