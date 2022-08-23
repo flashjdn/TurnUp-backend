a89c8e344fceb82cc16c5485fea5b12341a9ea4e
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2022-08-23 16:20:52 +0100

    model created

--- models/events.js
+++ models/events.js
@@ -105,6 +105,19 @@ export async function createUserEvents(request) {
   return result.rows;
 }
 
+export async function deleteUserEvent(eventId, userId) {
+  const result = await query(
+    `
+        DELETE FROM userEvents
+        WHERE eventId = $1 AND userId= $2;`,
+    [eventId, userId]
+  );
+  if (result.rowCount === 0) {
+    return `No userEvent found with ID: ${eventId}`;
+  }
+  return result.command;
+}
+
 // ************** Delete Request **************************************
 export async function deleteEvent(eventId) {
   const result = await query(
